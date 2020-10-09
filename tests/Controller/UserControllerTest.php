175320f681bab87e0d09d9d19efc750df1d5e4e3
LEVEQUE <alexis.leveque78@gmail.com>
2020-10-09 14:50:12 +0200

    testEditUser is ok #6

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -16,8 +16,8 @@ class UserControllerTest extends WebTestCase
         $client = $this->CreateClient();
 
         $userRepository = static::$container->get(UserRepository::class);
-        $testUser = $userRepository->findOneBy(['username' => 'Admin']);
-        $client->loginUser($testUser);
+        $adminTest = $userRepository->findOneBy(['username' => 'Admin']);
+        $client->loginUser($adminTest);
 
         $crawler = $client->request('GET', '/users/create');
 
@@ -35,6 +35,37 @@ class UserControllerTest extends WebTestCase
         $this->assertInstanceOf(User::class,$userTest);
         $this->assertSame('UserTest',$userTest->getUsername());
         $this->assertSame('ROLE_USER',$userTest->getRoles()[0]);
+
+    }
+
+    public function testEditUser()
+    {
+        $client = $this->CreateClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $adminTest = $userRepository->findOneBy(['username' => 'Admin']);
+        $client->loginUser($adminTest);
+
+        $crawler = $client->request('GET', '/users/2/edit');
+
+        $this->assertSame(1, $crawler->filter('h1.userEdition')->count());
+
+        $testUserBefore = $userRepository->findOneBy(['id'=>'2']);
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['user[username]'] = 'UserTest';
+        $form['user[password][first]'] = 'test';
+        $form['user[password][second]'] = 'test';
+        $form['user[email]'] = 'test@test.com';
+//        $form['user[roles][]'] = "ROLE_ADMIN";
+        $client->submit($form);
+
+        $testUserAfter = $userRepository->findOneBy(['id'=>'2']);
+
+        $this->assertInstanceOf(User::class,$testUserAfter);
+        $this->assertNotSame($testUserAfter->getUsername(),$testUserBefore->getUsername());
+        $this->assertSame('UserTest',$testUserAfter->getUsername());
+//        $this->assertSame('ROLE_ADMIN',$testUserAfter->getRoles()[0]);
 
     }
 
